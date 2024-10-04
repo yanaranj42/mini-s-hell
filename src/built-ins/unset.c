4f1f44f97d0b121320288f1ff11e4d1099f07ec6
Yaja <yaja@MacBook-Pro-de-Rixi.local>
2024-10-04 11:53:02 +0200

    unset ok

--- src/built-ins/unset.c
+++ src/built-ins/unset.c
@@ -3,89 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   unset.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yanaranj <yanaranj@student.42.fr>          +#+  +:+       +#+        */
+/*   By: yaja <yaja@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/26 14:20:52 by yanaranj          #+#    #+#             */
-/*   Updated: 2024/10/01 14:01:35 by yanaranj         ###   ########.fr       */
+/*   Updated: 2024/10/04 11:52:37 by yaja             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 #include "libft.h"
 
-/* Este builtin borra las variables de entorno que elijamos y modifica tambien
-    la lista que tengamos en el env que se vean afectadas por esta. (ex. CD,
-	OWD, OLDPWD...)
-    *-Si la variabe no existe, no hace nada, elimina el resto de variables y
-	vuelve al prompt
-    
-    --> puedo recibir + variables en la misma linea para eliminar
-
-	argv[0] --> unset
-	argv[1] --> variable
-*/
-/*
-	no se esta moviendo correctamente la lista despues de eliminar la var eencontrada.
-	mirar de hacerlo con iteradores
-*/
-/* void	do_unset(t_general *data, char *var)
-{
-	t_env	*tmp;
-	t_env	*new_env;
-	
-	tmp = data->env_lst;
-	new_env = NULL;
-	while (tmp != NULL)
-	{
-		printf("env var: %s\tparam var: %s\n", tmp->name, var);
-		if (ft_strncmp(tmp->name, var, ft_strlen(var)) == 0)
-		{
-			printf("env var: %s\tparam var: %s\n", tmp->name, var);
-			printf("%s\t Hay match y procedo a eliminarla\n", var);
-			data->env_lst = data->env_lst->next;
-			unset_free(tmp);
-			return ;
-		}
-		tmp = tmp->next;
-	}
-	printf("%s:\tMatch not found\n\n\n", var);
-	printf("Si no es el final y no encuentro la var, copiamos esta en el nodo\n");
-	printf("actual y nos movemos al siguiente\n");
-	while (tmp != NULL && ft_strncmp(tmp->name, var, ft_strlen(var)) != 0)
-	{
-		new_env = tmp;
-		tmp = tmp->next;
-	}
-	if (tmp == NULL)
-		return ;
-	new_env->next = tmp->next;
-	unset_free(tmp);
-}
-int    ft_unset(t_general *data)
-{
-    t_env   *env;
-    char	**argv;
-	int		i;
-
-	i = 1;
-	argv = data->first_token->argv;
-	env = data->env_lst;
-	if (!argv[i])//variable name
-		;
-	while (argv[i])
-	{
-		printf("param recibido: %s\n", argv[i]);
-		do_unset(data, argv[i]);
-		i++;
-	}
-	if (data->env_matrix)
-		arr_clean(data->env_matrix);
-	printf("Aqui actualizamos la lst del env:\n");
-	//get_matrix_env(data, data->env_lst);
-	if (!data->env_matrix)
-		return (error_brk(data, ERR01, NULL, 12));
-	return (OK);
-} */
 //SEGUNDA VERSION
 
 void	do_unset(t_general *data, char *var)
@@ -129,8 +56,9 @@ int	ft_unset(t_general *data)
 		do_unset(data, argv[i]);
 		i++;
 	}
-	printf("he salid del do_unset\n\n");
-	 
+	if (data->env_matrix)
+		arr_clean(data->env_matrix);
+	get_matrix_env(data, head);
 	return (OK);
 }
 
